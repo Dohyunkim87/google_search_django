9ccb6c75c5da9cb38a5828abc1fcf74d79c33407
Dohyunim87 <dohyunkim009@gmail.com>
2024-05-29 20:48:52 +0900

    allowed host add

--- google_search_django/settings.py
+++ google_search_django/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-@d+g!d$kwk9-3qt-_1kk#su*l2xbcl+bn0w3y^@#w676h^&jr7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
