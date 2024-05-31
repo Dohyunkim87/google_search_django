7519d73ea2b81483378ee217c2658fb041d49cf8
Dohyunim87 <dohyunkim009@gmail.com>
2024-05-31 10:08:44 +0900

    Django Secret Key Hide

--- google_search_django/settings.py
+++ google_search_django/settings.py
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@d+g!d$kwk9-3qt-_1kk#su*l2xbcl+bn0w3y^@#w676h^&jr7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
